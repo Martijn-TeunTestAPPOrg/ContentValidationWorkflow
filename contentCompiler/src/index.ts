d2d819a838b549fbcf36b24cf2e08b5752735e27
Teunvdk <teunvanderkleij@gmail.com>
2025-01-23 13:30:43 +0100

    tot en met stap 6 af, errors bij stap 7

--- contentCompiler/src/index.ts
+++ contentCompiler/src/index.ts
@@ -17,11 +17,14 @@ export default (app: Probot) => {
       const repoOwner = payload.repository.owner.login;
       const repoName = payload.repository.name;
       const repoUrl = payload.repository.clone_url;
+      const tempContentFolder = 'src/temp_content'
+      const tempStorageFolder = 'src/temp_storage';
 
       const __dirname = process.cwd();
       context.log.info(`Push event received for ${repoOwner}/${repoName}`);
       const cloneDirectory = path.join(__dirname, "src/temp_content");
 
+      ///
       try {
         if (fs.existsSync(cloneDirectory)) {
           deleteFolderRecursiveSync(cloneDirectory)
@@ -33,6 +36,8 @@ export default (app: Probot) => {
       catch (error: any) {
         context.log.error(`Failed to clone repository: ${error.message}`);
       }
+
+      ///
       try {
         context.log.info(`Checking out to the 'content' branch...`);
         await git.cwd(cloneDirectory).checkout("content");
@@ -42,15 +47,113 @@ export default (app: Probot) => {
       catch (error: any) {
         context.log.error(`Failed to clone and checkout repository: ${error.message}`);
       }
-    }
-    exec('python3 src/scripts/script.py', (error: any, stdout: any) => {
-      if (error) {
-        console.error(`Execution error: ${error}`);
-        return;
+      exec('python3 src/scripts/compile_content.py', (error: any, stdout: any) => {
+        if (error) {
+          context.log.error(`Execution error: ${error.message}`);
+          return;
+        }
+        context.log.info(`Output: ${stdout}`);
+      });
+
+      const sourceFiles = ['taxco_report.md', 'content_report.md'];
+      await waitForFiles(path.join(__dirname, tempContentFolder, sourceFiles[0]))
+
+      sourceFiles.forEach((file) => {
+        const sourcePath = path.join(__dirname, tempContentFolder, file);
+        const destinationPath = path.join(__dirname, tempStorageFolder, file);
+        fs.copyFileSync(sourcePath, destinationPath);
+      });
+
+      ///
+      const sourceBuildDir = path.join(__dirname, tempContentFolder, 'build');
+      const destinationBuildDir = path.join(__dirname, tempStorageFolder, 'build');
+
+      if (fs.existsSync(destinationBuildDir)) {
+        deleteFolderRecursiveSync(destinationBuildDir)
       }
-      console.log(`Output: ${stdout}`);
-    });
+      fs.mkdirSync(destinationBuildDir);
+
+      fs.readdirSync(sourceBuildDir).forEach((file) => {
+        const sourcePath = path.join(sourceBuildDir, file);
+        const destinationPath = path.join(destinationBuildDir, file);
+
+        if (fs.lstatSync(sourcePath).isDirectory()) {
+          context.log.info(sourcePath)
+          fs.cpSync(sourcePath, destinationPath, { recursive: true });
+        } else {
+          fs.copyFileSync(sourcePath, destinationPath);
+        }
+      });
+      deleteFolderRecursiveSync(sourceBuildDir)
+
+      try {
+        await git.add(['content_report.md', 'taxco_report.md'])
+        const status = await git.status()
+        if (!status.isClean()) {
+          await git.commit("Update reports")
+          await git.push('origin', 'content')
+          context.log.info('Reports committed and pushed to content branch');
+        }
+        else {
+          context.log.info('No changes to commit');
+        }
+      }
+      catch (error: any) {
+        context.log.error(`Failed to commit and push reports: ${error.message}`);
+      }
+
+      // try {
+      //   context.log.info(`Checking out to the 'staging' branch...`);
+      //   await git.cwd(cloneDirectory).checkout("content");
+
+      //   if (fs.existsSync(sourceBuildDir)) {
+      //     deleteFolderRecursiveSync(sourceBuildDir)
+      //   }
+      //   fs.mkdirSync(sourceBuildDir)
+
+      //   fs.readdirSync(destinationBuildDir).forEach((file) => {
+      //     const sourcePath = path.join(destinationBuildDir, file);
+      //     const destinationPath = path.join(sourceBuildDir, file);
+
+      //     if (fs.lstatSync(sourcePath).isDirectory()) {
+      //       context.log.info(sourcePath)
+      //       fs.cpSync(sourcePath, destinationPath, { recursive: true });
+      //     } else {
+      //       fs.copyFileSync(sourcePath, destinationPath);
+      //     }
+      //   });
+      //   deleteFolderRecursiveSync(destinationBuildDir)
+
+      //   await git.add('build/')
+
+      //   const sourceFiles = ['taxco_report.md', 'content_report.md'];
+
+      //   sourceFiles.forEach((file) => {
+      //     const sourcePath = path.join(__dirname, tempStorageFolder, file);
+      //     const destinationPath = path.join(__dirname, tempContentFolder, file);
+      //     fs.copyFileSync(sourcePath, destinationPath);
+      //   });
+      //   await git.add('taxco_report.md')
+      //   await git.add('content_report.md')
+
+      //   const statusBeforeCommit = await git.status();
+      //   if (!statusBeforeCommit.isClean()) {
+      //     await git.commit('Sync compiled files and reports to staging');
+      //     await git.push('origin', 'staging')
+      //   } else {
+      //     context.log.info('No changes to commit');
+      //   }
+
+      // }
+      // catch (error: any) {
+      //   context.log.error(`Failed to sync to staging branch: ${error.message}`);
+      // }
+
+
+
+    }
   });
+
   const deleteFolderRecursiveSync = (folderPath: string) => {
     if (fs.existsSync(folderPath)) {
       const files = fs.readdirSync(folderPath);
@@ -67,42 +170,19 @@ export default (app: Probot) => {
       app.log.warn(`Folder ${folderPath} does not exist`);
     }
   }
+  const waitForFiles = async (sourcePath: any) => {
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+    let attempts = 5;
+    while (attempts > 0) {
+      if (fs.existsSync(sourcePath)) {
+        console.log('File is ready:', sourcePath);
+        return true;
+      }
+      console.log(`Waiting for file to be available... (${5 - attempts + 1}/5)`);
+      await delay(1000);
+      attempts--;
+    }
+    throw new Error('File did not become available in time.');
+  }
 };
-
-
-
-// - name: Copy reports to root
-// run: |
-// cp content_report.md ../
-// cp taxco_report.md ../
-// # Step 6: Move build folder to root
-// - name: Move build folder to root
-// run: |
-// mkdir -p ../build && cp -r build/* ../build/ && rm -rf build
-// # Step 7: Commit report.md to content branch
-// - name: Commit report.md to content branch
-// run: |
-// git add content_report.md
-// git add taxco_report.md
-// if ! git diff-index --quiet HEAD; then
-//   git commit -m "Update reports"
-//   git push origin content
-// else
-//   echo "No changes to commit"
-// fi
-// # Step 8: Sync to staging branch
-// - name: Switch to staging branch and sync
-// run: |
-// git fetch origin
-// git checkout staging
-// git rm -rf build/ || true
-// cp -r ../build/ build
-// git add build/
-// cp ../content_report.md content_report.md
-// cp ../taxco_report.md taxco_report.md
-// git add content_report.md
-// git add taxco_report.md
-// git commit -m "Sync compiled files and reports to staging" || echo "No changes to commit"
-// git push origin staging
-
 
